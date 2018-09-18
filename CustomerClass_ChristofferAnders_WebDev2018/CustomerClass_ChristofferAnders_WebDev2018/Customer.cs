6a7a5f354da6690f2294596f6cced14dd8f026a1
Loopmootin <christoffera@outlook.dk>
2018-09-18 12:57:34 +0200

    First Commit
    Code for creating a Customer Class

---
+++ CustomerClass_ChristofferAnders_WebDev2018/CustomerClass_ChristofferAnders_WebDev2018/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerClass_ChristofferAnders_WebDev2018
+{
+    class Customer
+    {
+        private string Name;
+        private int Age;
+        private int ZipCode;
+        private string Password;
+
+        public Customer(string CostumerName, int CostumerAge, int CostumerZipCode, string CostumerPassword)
+        {
+            this.Name = CostumerName;
+            this.Age = CostumerAge;
+            this.ZipCode = CostumerZipCode;
+            this.Password = CostumerPassword;
+        }
+
+        public override string ToString()
+        {
+            return "Name: " + Name + "\nAge: " + Age + "\nZip: " + ZipCode + "\nPassword: " + Password;
+        }
+    }
+}
---
+++ CustomerClass_ChristofferAnders_WebDev2018/CustomerClass_ChristofferAnders_WebDev2018/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerClass_ChristofferAnders_WebDev2018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer MyCustomer = new Customer("Christoffer Andersen", 28, 3460, "miauw");
+
+            Console.WriteLine(MyCustomer);
+        }
+    }
+}
